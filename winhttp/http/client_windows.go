0a5e0667d174fefbccc79552f209b9740481c57a
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-05-14 17:40:05 +0800

    Update client_windows.go

--- winhttp/http/client_windows.go
+++ winhttp/http/client_windows.go
@@ -14,7 +14,7 @@ func NewClient() (*Client, error) {
 
 	// Create session
 	c.hndl, e = winhttp.Open(
-		"Go-http-client/1.1",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
 		winhttp.WinhttpAccessTypeAutomaticProxy,
 		"",
 		"",
